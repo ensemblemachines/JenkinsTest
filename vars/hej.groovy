3d6bb06c91de61b92c554d9eaeeaeb1e5826c052
ensemblemachines <thieugiaho_ngo@yahoo.com>
2020-11-16 15:13:30 +0100

    edited hej

--- vars/hej.groovy
+++ vars/hej.groovy
@@ -1,7 +1,7 @@
 
 def call() {
 	println "Hej"
-	sh("python3 /Users/henrikpersson/.jenkins/workspace/Pipeline Job@libs/JENKINS_TEST/vars/Pytest/test_capitalize.py")
+	sh("python3 '/Users/henrikpersson/.jenkins/workspace/Pipeline Job@libs/JENKINS_TEST/vars/Pytest/test_capitalize.py' ")
 	//archiveArtifacts artifacts: '**', fingerprint: true
 }
 
